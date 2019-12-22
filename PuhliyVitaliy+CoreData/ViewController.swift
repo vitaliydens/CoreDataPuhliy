1172737225cfb1c827dc93ca2a4b4c0eaa941ecd
Anton Skripko <ant.skripko@gmail.com>
2019-12-22 12:11:17 +0200

    Initial Commit

---
+++ PuhliyVitaliy+CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PuhliyVitaliy+CoreData
+//
+//  Created by Developer on 22.12.2019.
+//  Copyright © 2019 Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
